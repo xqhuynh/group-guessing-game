70d814f5149f72b9c9cb187e1760f71f6eb949d6
Savon Huynh <xqhuynh@gmail.com>
2022-06-01 20:12:19 -0500

    Create POST call

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,35 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!");
+  console.log(randomNumber);
+  $(".submitBtn").on("click", submitBtnHandler);
 }
+
+function submitBtnHandler() {
+  let num1 = $(".numberInput1").val();
+  let num2 = $(".numberInput2").val();
+  let numberObject = {
+    num1: num1,
+    num2: num2,
+  };
+
+  // Ajax POST call
+  $.ajax({
+    method: "POST",
+    url: "/game",
+    data: numberObject,
+  })
+    .then((response) => {
+      console.log("in submitBtnHandler", response);
+      // Refresh data after submit button clicked
+      getNumbers();
+      // Clear inputs after submit
+      $(".numberInput1").val("");
+      $(".numberInput2").val("");
+    })
+    .catch((err) => {
+      console.log("POST function failed", err);
+    });
+}
+
+function getNumbers() {}
--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static files (HTML, CSS, Client JS)
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
+
+const randomNumber = Math.floor(Math.random() * (25 - 1 + 1) + 1);
 
 // GET & POST Routes go here
-
+app.post("/game", (req, res) => {
+  let newNumbers = req.body;
+  console.log("New numbers added", newNumbers);
+});
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+  console.log("Server is running on port", PORT);
+});
